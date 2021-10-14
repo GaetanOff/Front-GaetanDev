2be0bdc5cc97682ddbea037b7563f413923049a8
Gaëtan <61387206+GaetanOff@users.noreply.github.com>
2021-10-14 15:21:54 +0200

    Reformat & Added achievement

--- karma.conf.js
+++ karma.conf.js
@@ -28,8 +28,8 @@ module.exports = function (config) {
       dir: require('path').join(__dirname, './coverage/Front-Gaetan2mz-V2'),
       subdir: '.',
       reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
+        {type: 'html'},
+        {type: 'text-summary'}
       ]
     },
     reporters: ['progress', 'kjhtml'],
--- src/app/components/base/achievements/javaplugins/javaplugins.component.spec.ts
+++ src/app/components/base/achievements/javaplugins/javaplugins.component.spec.ts
@@ -1,6 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { JavapluginsComponent } from './javaplugins.component';
+import {JavapluginsComponent} from './javaplugins.component';
 
 describe('JavapluginsComponent', () => {
   let component: JavapluginsComponent;
@@ -8,9 +8,9 @@ describe('JavapluginsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ JavapluginsComponent ]
+      declarations: [JavapluginsComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
