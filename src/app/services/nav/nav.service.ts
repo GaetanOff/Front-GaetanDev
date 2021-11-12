15f590dbf7be0ea792ba4e5facc1936920a4c2f4
Gaetan <gaetanfaucher1301@gmail.com>
2021-11-12 16:17:54 +0100

    Update nav.service.ts

--- src/app/services/nav/nav.service.ts
+++ src/app/services/nav/nav.service.ts
@@ -8,34 +8,15 @@ export class NavService {
   constructor() { }
 
   updateNav(): void {
-    // @ts-ignore
-    if (document.getElementById("contact").classList.contains("font-bold")) {
-      // @ts-ignore
-      document.getElementById("contact").classList.toggle("font-bold");
-    }
+    let pageList = ['contact', 'real', 'about', 'accueil', 'blog'];
 
-    // @ts-ignore
-    if (document.getElementById("real").classList.contains("font-bold")) {
+    pageList.forEach(page => {
       // @ts-ignore
-      document.getElementById("real").classList.toggle("font-bold");
-    }
+      if (document.getElementById(page).classList.contains("font-bold")) {
+        // @ts-ignore
+        document.getElementById(page).classList.toggle("font-bold");
+      }
+    })
 
-    // @ts-ignore
-    if (document.getElementById("about").classList.contains("font-bold")) {
-      // @ts-ignore
-      document.getElementById("about").classList.toggle("font-bold");
-    }
-
-    // @ts-ignore
-    if (document.getElementById("accueil").classList.contains("font-bold")) {
-      // @ts-ignore
-      document.getElementById("accueil").classList.toggle("font-bold");
-    }
-
-    // @ts-ignore
-    if (document.getElementById("blog").classList.contains("font-bold")) {
-      // @ts-ignore
-      document.getElementById("blog").classList.toggle("font-bold");
-    }
   }
 }
