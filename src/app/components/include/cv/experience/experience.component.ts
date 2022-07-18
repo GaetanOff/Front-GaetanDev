86f87b9cfb063023df8d8fcbd8e60e553ac0e700
Gaetan <gaetanfaucher1301@gmail.com>
2022-07-18 22:17:15 +0200

    Update experience.component.ts

--- src/app/components/include/cv/experience/experience.component.ts
+++ src/app/components/include/cv/experience/experience.component.ts
@@ -8,7 +8,7 @@ import {I18nService} from "../../../../services/i18n/i18n.service";
 export class ExperienceComponent implements OnInit {
   experiencesList: any = [
     {
-      name: 'Free-lance',
+      name: 'Freelance',
       date: this.i18n.isFrench ?
         "October 2016 - Today" :
         "Octobre 2016 - Aujourd'hui",
@@ -18,10 +18,30 @@ export class ExperienceComponent implements OnInit {
       active: true
     },
     {
+      name: 'FirstSky',
+      date: this.i18n.isFrench ?
+        "September 2021 - Today" :
+        "Septembre 2021 - Aujourd'hui",
+      description: this.i18n.isFrench ?
+        'Management of the entire server infrastructure' :
+        "Gestion de l'infrastructure de l'ensemble du serveur",
+      active: true
+    },
+    {
       name: 'Ubisoft',
       date: this.i18n.isFrench ?
-        'January 2019' :
-        'Janvier 2019',
+        'January 2020' :
+        'Janvier 2020',
+      description: this.i18n.isFrench ?
+        'Internship: Software Architect & DevOps' :
+        'Stage: Architecte logiciel & DevOps',
+      active: false
+    },
+    {
+      name: 'Université de Montpellier',
+      date: this.i18n.isFrench ?
+        'January 2019 / January 2020' :
+        'Janvier 2019 / Janvier 2020',
       description: this.i18n.isFrench ?
         'Internship: Software Architect & DevOps' :
         'Stage: Architecte logiciel & DevOps',
