6e3160f3bbe005197538c3f262077870bd161169
Gaëtan <61387206+GaetanOff@users.noreply.github.com>
2025-03-14 18:03:47 +0100

    added loading effect

--- src/app/components/base/admin/proxies/proxies.component.ts
+++ src/app/components/base/admin/proxies/proxies.component.ts
@@ -1,38 +1,67 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {TempladminComponent} from "../../../include/admin/templadmin/templadmin.component";
 import {CommonModule, NgFor, NgIf} from "@angular/common";
 import {AdminService} from "../../../../services/admin/admin.service";
+import {interval, Subject} from "rxjs";
+import {takeUntil} from "rxjs/operators";
+import {toast} from 'ngx-sonner';
+import {
+  RefreshProxiesWhitelistComponent
+} from "../../../include/skeletons/refresh-proxies-whitelist/refresh-proxies-whitelist.component";
 
 @Component({
   selector: 'app-proxies',
   imports: [
     NgFor,
+    NgIf,
     CommonModule,
-    TempladminComponent
+    TempladminComponent,
+    RefreshProxiesWhitelistComponent
   ],
   templateUrl: './proxies.component.html'
 })
-export class ProxiesComponent {
+export class ProxiesComponent implements OnInit, OnDestroy {
   httpProxies: string[] = [];
   socks5Proxies: string[] = [];
   lastRefresh: string = '';
   isLoading: boolean = false;
+  private unsubscribe$ = new Subject<void>();
+  protected readonly toast = toast;
 
   constructor(private adminService: AdminService) {
-    this.refreshProxies();
   }
 
-  refreshProxies(): void {
+  ngOnInit() {
+    this.refreshProxies().catch(() => this.toast.error("Failed to refresh whitelist."));
+
+    interval(60000)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(() => this.refreshProxies());
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+  async refreshProxies(): Promise<void> {
+    if (this.isLoading) return;
+
     this.isLoading = true;
+    const loadingToast: string | number = this.toast.loading("Refreshing proxies...");
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
     this.adminService.getProxies()
       .subscribe({
         next: response => {
           this.httpProxies = response.http;
           this.socks5Proxies = response.socks5;
           this.lastRefresh = response.lastRefresh;
+          this.toast.success("Proxies refreshed", {id: loadingToast});
           this.isLoading = false;
         },
         error: () => {
+          this.toast.error("Failed to refresh whitelist.");
           this.isLoading = false;
         }
       });
